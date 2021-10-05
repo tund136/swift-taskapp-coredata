c62d6c63c04e5cdfd8e2d2cb76e0d0f1cd466212
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-05 09:27:12 +0700

    Create NewDataView

---
+++ TaskAppCoreData/Views/NewDataView.swift
@@ -0,0 +1,17 @@
+//
+//  NewDataView.swift
+//  TaskAppCoreData
+//
+//  Created by Danh Tu on 05/10/2021.
+//
+
+import SwiftUI
+
+struct NewDataView: View {
+    @ObservedObject var homeData: HomeViewModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
