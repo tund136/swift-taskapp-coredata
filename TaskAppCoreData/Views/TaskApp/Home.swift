07cec9cbe0dc07e156a87a01c7437de5fd5aca77
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-05 12:04:45 +0700

    Reading Data and create Add New Task button and Context Menu: Edit and Delete for each tasks

--- TaskAppCoreData/Views/TaskApp/Home.swift
+++ TaskAppCoreData/Views/TaskApp/Home.swift
@@ -6,18 +6,80 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct Home: View {
     @StateObject var homeData = HomeViewModel()
     
+    // Fetching Data
+    @FetchRequest(entity: Task.entity(), sortDescriptors: [NSSortDescriptor(key: "date", ascending: true)], animation: .spring()) var results: FetchedResults<Task>
+    
     var body: some View {
-        Button(action: {
-            homeData.isNewData.toggle()
-        }, label: {
-            Text("Button")
-        })
-            .sheet(isPresented: $homeData.isNewData) {
-                NewDataView(homeData: homeData)
+        ZStack(alignment: Alignment(horizontal: .trailing, vertical: .bottom)) {
+            VStack {
+                
+                HStack {
+                    Text("Tasks")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(.black)
+                    
+                    Spacer()
+                }
+                .padding()
+                .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                .background(Color.white)
+                
+                ScrollView(.vertical, showsIndicators: false) {
+                    LazyVStack(alignment: .leading, spacing: 20) {
+                        ForEach(results) { task in
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text(task.content ?? "")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                
+                                Text(task.date ?? Date(), style: .date)
+                                    .fontWeight(.bold)
+                            }
+                            .foregroundColor(.black)
+                            .contextMenu {
+                                Button(action: {
+                                    
+                                }, label: {
+                                    Text("Edit")
+                                })
+                                
+                                Button(action: {
+                                    
+                                }, label: {
+                                    Text("Delete")
+                                })
+                            }
+                        }
+                    }
+                    .padding()
+                }
             }
+            
+            // Add Button
+            Button(action: {
+                homeData.isNewData.toggle()
+            }, label: {
+                Image(systemName: "plus")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(20)
+                    .background(
+                        AngularGradient(gradient: .init(colors: [Color(.systemOrange), Color(.systemRed)]), center: .center)
+                    )
+                    .clipShape(Circle())
+            })
+                .padding()
+        }
+        .ignoresSafeArea(.all, edges: .top)
+        .background(Color.black.opacity(0.06).ignoresSafeArea(.all, edges: .all))
+        .sheet(isPresented: $homeData.isNewData) {
+            NewDataView(homeData: homeData)
+        }
     }
 }
