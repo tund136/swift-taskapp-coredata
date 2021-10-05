e1c63aae53ba56b0379c081a4b94ff96568aa486
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-05 09:39:54 +0700

    Architecture

---
+++ TaskAppCoreData/Views/Helpers/DateButton.swift
@@ -0,0 +1,20 @@
+//
+//  DateButton.swift
+//  TaskAppCoreData
+//
+//  Created by Danh Tu on 05/10/2021.
+//
+
+import SwiftUI
+
+struct DateButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DateButton_Previews: PreviewProvider {
+    static var previews: some View {
+        DateButton()
+    }
+}
